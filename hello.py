183fbddb5b7f05b673139c04fb251b7c48796506
Abdul-Hanan-0712 <20098126@mydbs.ie>
2026-06-17 11:31:09 +0100

    nano hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
-@app.route("/")#URL leading to method
+
+@app.route("/") # URL leading to method
 def hello(): # Name of the method
- return("Hello World!") #indent this line
+    return("Hello World!") # Indented by 4 spaces
+
 if __name__ == "__main__":
- app.run(host='0.0.0.0',port='8080', ssl_context=('cert.pem', 'privkey.pem')
+    # Added the closing parenthesis at the end here:
+    app.run(host='0.0.0.0', port='8080', ssl_context=('cert.pem', 'privkey.pem'))
